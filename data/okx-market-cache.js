f76b5bbb60272c4670c6ea0b1b846eb5c045851f
Fenhwy <evaggo22222@gmail.com>
2026-07-29 20:35:06 +0800

    Add OKX cache placeholders

---
+++ data/okx-market-cache.js
@@ -0,0 +1,9 @@
+window.OKX_MARKET_SNAPSHOT = Object.freeze({
+  version: 1,
+  source: "OKX public market data",
+  generatedAt: null,
+  tickerUpdatedAt: null,
+  tickers: [],
+  candles: {},
+  candleUpdatedAt: {}
+});
